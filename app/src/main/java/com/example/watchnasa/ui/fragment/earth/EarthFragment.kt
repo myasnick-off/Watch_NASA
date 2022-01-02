fb3ceafc7d70b9eef43f3762929e2ceb19052ef8
myasnick-off <cyber.jokep@gmail.com>
2022-01-02 16:55:27 +0300

    Урок 5. Добавил анимацию на вкладке Земли: увеличение фото и скрытие описания по нажатию на фото.

--- app/src/main/java/com/example/watchnasa/ui/fragment/earth/EarthFragment.kt
+++ app/src/main/java/com/example/watchnasa/ui/fragment/earth/EarthFragment.kt
@@ -1,12 +1,17 @@
 package com.example.watchnasa.ui.fragment.earth
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.transition.*
 import coil.load
 import com.example.watchnasa.BuildConfig
 import com.example.watchnasa.databinding.FragmentEarthBinding
@@ -26,6 +31,8 @@ class EarthFragment : Fragment() {
     private var _binding: FragmentEarthBinding? = null
     private val binding: FragmentEarthBinding
         get() = _binding!!
+    // флаг состояния масшатба фото Земли
+    private var isZoomed = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,11 +43,37 @@ class EarthFragment : Fragment() {
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
         super.onViewCreated(view, savedInstanceState)
         val observer = Observer<EpicState> { renderData(it) }
         viewModel.getLiveData().observe(viewLifecycleOwner, observer)
         viewModel.getEpicImageFromServer()
+
+        // задаем анимацию (увеличение фото и скрытие описания) по нажатию на изображение
+        earthPhotoView.setOnClickListener {
+            isZoomed = !isZoomed
+            val cbTransition = ChangeBounds().apply { duration = 500 }
+            val imageTransition = ChangeImageTransform().apply { duration = 500 }
+            val fadeTransition = Fade().apply { duration = 500 }
+            val transitionSet = TransitionSet().apply {
+                addTransition(cbTransition)
+                addTransition(imageTransition)
+                addTransition(fadeTransition)
+            }
+            TransitionManager.beginDelayedTransition(earthContainer, transitionSet)
+            // приводим параметры картинки к праметрам контейнера, чтобы добраться до ее высоты
+            val params = earthPhotoView.layoutParams as ConstraintLayout.LayoutParams
+
+            if (isZoomed) {
+                params.height = ConstraintLayout.LayoutParams.MATCH_PARENT
+                earthPhotoView.scaleType = ImageView.ScaleType.CENTER_CROP
+                earthDataCard.root.hide()
+            } else {
+                params.height = ConstraintLayout.LayoutParams.WRAP_CONTENT
+                earthPhotoView.scaleType = ImageView.ScaleType.FIT_CENTER
+                earthDataCard.root.show()
+            }
+        }
     }
 
     override fun onDestroy() {
